e1c0b902b951e8ed5b40ec86620ed3755c36beae
tienanh <tienanh12108991@gmail.com>
2020-04-22 15:41:00 +0700

    up

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+class Application {
+    private static $instance;
+
+    public static function getInstance() {
+        if(self::$instance === null) {
+            self::$instance = new Application();
+        }
+        return self::$instance;
+    }
+}
+
+$app1 = Application::getInstance();
+$app2 = Application::getInstance();
